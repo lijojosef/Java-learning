e6c6b4cda513c6406079fd5bc2fe7f3588860094
Lijo Joseph <lijojoseph234@gmail.com>
2022-03-12 06:29:50 +0530

    Variables

---
+++ O3Variable.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ * @author lijoj
+ *
+ * Variable in Java is a data container that saves the data values during Java program execution. 
+ * Every variable is assigned a data type that designates the type and quantity of value it can hold. 
+ * Variable is a memory location name of the data.
+ */
+
+public class O3Variable {
+
+	//	Static Variables
+	/*
+	 * created when an object of the class is created and destroyed when the object
+	 * is destroyed.
+	 */
+	static int a;
+
+	//	Instance Variables
+
+	int b;
+
+	public static void main(String[] args) 
+	{
+		/*
+		 * A variable is initialized by stating the following.
+		 * Datatype: Type of data that can be stored in this variable.
+		 * Dataname: Name was given to the variable.
+		 */
+		//		Local Variable 
+		int count;
+
+		/* Initialize with value */
+		int time = 10, speed = 20;
+
+	}
+}
