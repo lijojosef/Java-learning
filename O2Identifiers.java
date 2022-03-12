ca80dc0b53ec143b173157a57d622de3c8c3fe36
Lijo Joseph <lijojoseph234@gmail.com>
2022-03-12 06:16:14 +0530

    Identifiers

---
+++ O2Identifiers.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ * @author lijoj
+ *
+ * identifiers are used for identification purposes. 
+ * In Java, an identifier can be a class name, method name, variable name, or label
+ */
+
+public class O2Identifiers {
+	public static void main(String[] args) 
+	{
+		int a = 20;
+	}
+
+	/*
+	 * Identifiers: 
+	 * O2Identifiers : class name. 
+	 * main : method name. 
+	 * String : predefined class name.
+	 * args : variable name. 
+	 * a : variable name.
+	 */
+
+	/*
+	 * Rules The only allowed characters for identifiers are all alphanumeric
+	 * characters([A-Z],[a-z],[0-9]), ‘$‘(dollar sign) and ‘_‘ (underscore). For
+	 * example “geek@” is not a valid java identifier as it contain ‘@’ special
+	 * character. Identifiers should not start with digits([0-9]). For example
+	 * “123geeks” is a not a valid java identifier. Java identifiers are
+	 * case-sensitive. There is no limit on the length of the identifier but it is
+	 * advisable to use an optimum length of 4 – 15 letters only. Reserved Words
+	 * can’t be used as an identifier. For example “int while = 20;” is an invalid
+	 * statement as while is a reserved word. There are 53 reserved words in Java.
+	 */
+
+	/*
+	 * Reserved Words keywords(50) and literals(3).
+	 */
+}
