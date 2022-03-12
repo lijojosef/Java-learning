1fdf33a074d83583d18e419c6097a36e0ef7a697
Lijo Joseph <lijojoseph234@gmail.com>
2022-03-12 05:56:19 +0530

    WelcomeToMyTutorial

---
+++ WelcomeToMyTutorial.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ * @author lijoj
+ * Follow this tutorial series to brush up and master your skills in Java
+ */
+/*Importing all the classes of io package. 
+ * Package provides a set of input-output streams for reading and writing data to files or other input or output sources.*/
+
+import java.io.*;
+
+/*class contains the data and methods to be used in the program. 
+ * Methods define the behavior of the class.*/
+
+public class WelcomeToMyTutorial {
+
+	/*
+	 * static keyword tells us that this method is accessible without instantiating
+	 * the class.
+	 * keywords tell that this method will not return anything. 
+	 * The main() method is the entry point of our application.
+	 */ 
+
+	public static void main(String[] args)
+	{
+		/*
+		 * System.in : This is the standard input stream that is used to read characters
+		 * from the keyboard or any other standard input device. System.out : standard
+		 * output stream that is used to produce the result of a program on an output
+		 * device like the computer screen.
+		 */
+
+		/*
+		 * println(): This method in Java is also used to display text on the console.
+		 * It prints the text on the console and the cursor moves to the start of the
+		 * next line at the console. The next printing takes place from the next line.
+		 */
+
+		System.out.println("Welcome to my tutorial in Java");
+	}
+}
+/**
+ * Everything in java , is represented in Class as an object including the main function.
+ */
